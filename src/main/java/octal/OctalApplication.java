8d059f2047e4f8609a9f922973bd67d2c0c016e0
estuffin <erc.sutphen@gmail.com>
2020-07-05 21:29:00 -0400

    Initial commit

---
+++ src/main/java/octal/OctalApplication.java
@@ -0,0 +1,13 @@
+package octal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctalApplication.class, args);
+	}
+
+}
