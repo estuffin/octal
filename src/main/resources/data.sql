05135afe24f2ca33a3c68c254f4b677783a80724
estuffin <erc.sutphen@gmail.com>
2020-07-08 23:41:52 -0400

    change insert script to not insert if row already exists

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 insert into oct_users 
-	(user_id, email, create_date, update_date, login_count, last_login_date, curr_login_date, curr_ip, last_ip, do_api_key) 
-values 
-	(1, 'test@test.com', systimestamp, null, 0, null, null, null, null, null);
+	select * from (select 1 user_id, 'test@test.com' email, systimestamp create_date, null update_date, 0 login_count, null last_login_date, null curr_login_date, null curr_ip, null last_ip, null do_api_key)  
+	 where 1 > (select count(*) 
+	              from oct_users 
+	             where user_id = 1);
