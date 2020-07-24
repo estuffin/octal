e7c20b99cb042bdc39854a0b9701de300405df4f
estuffin <erc.sutphen@gmail.com>
2020-07-23 23:09:36 -0400

    Swap H2 for MySQL.  Fix logout success redirect.

--- src/main/java/octal/CustomLogoutSuccessHandler.java
+++ src/main/java/octal/CustomLogoutSuccessHandler.java
@@ -13,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -26,6 +28,8 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 	private DBService db;
 	
 	Logger logger = LoggerFactory.getLogger(CustomLogoutSuccessHandler.class);
+	
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
 	@Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -48,5 +52,7 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
             session.removeAttribute("username");
             session.removeAttribute("userEmail");
         }
+        
+        redirectStrategy.sendRedirect(request, response, "/");
     }
 }
--- src/main/java/octal/SecurityConfiguration.java
+++ src/main/java/octal/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -25,7 +26,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutSuccessHandler(logoutSuccessHandler)
-                .logoutSuccessUrl("/")
                 .permitAll()
                 .and()
                 .oauth2Login()
